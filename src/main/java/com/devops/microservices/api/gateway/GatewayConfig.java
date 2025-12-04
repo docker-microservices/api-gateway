cbcc8b8a0275d55702f4e0f067cec31d68030110
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2025-12-04 13:50:48 +0100

    Initial commit

---
+++ src/main/java/com/devops/microservices/api/gateway/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.devops.microservices.api.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("books_service", r -> r.path("/books/**")
+                        .uri("lb://BOOKS-SERVICE"))
+                .route("authors_service", r -> r.path("/authors/**")
+                        .uri("lb://AUTHORS-SERVICE"))
+                .build();
+    }
+}
