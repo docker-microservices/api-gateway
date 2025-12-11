6e119cdbf6ad07e9a5f5203b1caff84208340d21
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2025-12-11 12:58:18 +0100

    add CorsConfig

--- src/main/java/com/devops/microservices/api/gateway/config/GatewayConfig.java
+++ src/main/java/com/devops/microservices/api/gateway/config/GatewayConfig.java
@@ -1,4 +1,4 @@
-package com.devops.microservices.api.gateway;
+package com.devops.microservices.api.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
